7b4ab7c4f874cdde9e8949e8bef4dbe308c74d97
Wannes Meert <wannes.meert@cs.kuleuven.be>
2017-02-05 21:37:57 +0100

    Update

--- pyGPs/Demo/Clustering/demo_clustering.py
+++ pyGPs/Demo/Clustering/demo_clustering.py
@@ -17,14 +17,13 @@ import sys
 import pyGPs
 from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error
-from pyGPs.Demo.Clustering import pyGP_extension as GPE
+from pyGPs.Demo.Clustering import pyGP_extension as gpe
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 import logging
 import random
 from collections import namedtuple
-
 
 logger = logging.getLogger("pyGPs.clustering")
 
@@ -37,6 +36,10 @@ def calculate_rmse_gp(vector_x, vector_y, weighted=True, plot=False, context=Non
     :param vector_y: valueSet of the timeseries
     :param weighted: weight RMSE wrt variance of prediction
     :param plot: plot the expected function
+    :param context: (internal)
+    :param optimization_params:
+    :param signed: Add a sign to RMSE based on whether the prediction is on average higher or lower than the prediction
+    :param sample: Learn from sample of the data (int for min number, float for fraction, list for inidices)
     :returns: list(idx,rmse), hyperparams, model
     """
     if optimization_params is None:
@@ -44,80 +47,92 @@ def calculate_rmse_gp(vector_x, vector_y, weighted=True, plot=False, context=Non
     # setX = [preprocessing.scale(element )for element in vectorX]
     # setY = preprocessing.scale(vector_y, axis=1)
 
+    vector_y_train = vector_y
+    vector_x_train = vector_x
     if sample:
         if type(sample) == float:
-            logger.debug("Sample series for training")
+            logger.debug("Sample series for training (ratio)")
             vector_y_train = []
             vector_x_train = []
-            for idx in random.sample(range(len(vector_y)), k=int(len(vector_y)*sample)):
+            for idx in random.sample(range(len(vector_y)), k=int(len(vector_y) * sample)):
                 vector_y_train.append(vector_y[idx])
                 vector_x_train.append(vector_x[idx])
+        elif type(sample) == int:
+            logger.debug("Sample series for training (number)")
+            if len(vector_y) <= sample:
+                vector_y_train = vector_y
+                vector_x_train = vector_x
+            else:
+                vector_y_train = []
+                vector_x_train = []
+                for idx in random.sample(range(len(vector_y)), k=sample):
+                    vector_y_train.append(vector_y[idx])
+                    vector_x_train.append(vector_x[idx])
         elif type(sample) == list:
+            logger.debug("Sample series for training (indices)")
             vector_y_train = []
             vector_x_train = []
             for idx in sample:
                 vector_y_train.append(vector_y[idx])
                 vector_x_train.append(vector_x[idx])
-    else:
-        vector_y_train = vector_y
-        vector_x_train = vector_x
 
-    model = pyGPs.GPR()      # specify model (GP regression)
-    k = pyGPs.cov.Linear() + pyGPs.cov.RBF() # hyperparams will be set with optimizeHyperparameters method
+    model = pyGPs.GPR()  # specify model (GP regression)
+    k = pyGPs.cov.Linear() + pyGPs.cov.RBF()  # hyperparams will be set with optimizeHyperparameters method
     model.setPrior(kernel=k)
 
-    hyperparams, model2 = GPE.optimizeHyperparameters(
+    hyperparams, model2 = gpe.optimizeHyperparameters(
         optimization_params.get("initialHyperParameters", [0.0000001, 0.0000001, 0.0000001]),
         model, vector_x_train, vector_y_train,
-        bounds= optimization_params.get("bounds", [(None, 5), (None, 5), (None, 5)]),
-        method = optimization_params.get("method", 'L-BFGS-B'))
+        bounds=optimization_params.get("bounds", [(None, 5), (None, 5), (None, 5)]),
+        method=optimization_params.get("method", 'L-BFGS-B'))
     print('hyperparameters used:', hyperparams)
     # mean (y_pred) variance (ys2), latent mean (fmu) variance (fs2), log predictive prob (lp)
     y_pred, ys2, fm, fs2, lp = model2.predict(vector_x[0])
     last_vector_x = vector_x[0]
 
-    rmseData = []
+    rmse_data = []
     for i in range(len(vector_y)):
         if not np.all(np.equal(last_vector_x, vector_x[i])):
             logger.debug("Recomputing prediction")
             y_pred, ys2, fm, fs2, lp = model2.predict(vector_x[i])
             last_vector_x = vector_x[i]
         if weighted:
-            rmse = math.sqrt(mean_squared_error(vector_y[i], y_pred, (np.max(ys2) - ys2))/np.max(ys2))
+            rmse = math.sqrt(mean_squared_error(vector_y[i], y_pred, (np.max(ys2) - ys2)) / np.max(ys2))
         else:
             rmse = math.sqrt(mean_squared_error(vector_y[i], y_pred))
         if signed:
             if np.mean(vector_y[i] - y_pred) < 0:
                 rmse = -rmse
-        rmseData.append((i, rmse))
+        rmse_data.append((i, rmse))
 
     if plot:
-        fig, ax = plt.subplots(nrows=2, ncols=1)
+        fig, ax = plt.subplots(nrows=1, ncols=2)
         xs = vector_x[0]
         ym = y_pred
         xss = np.reshape(xs, (xs.shape[0],))
         ymm = np.reshape(ym, (ym.shape[0],))
         ys22 = np.reshape(ys2, (ys2.shape[0],))
         for i in vector_y:
-            ax[0].plot(i,color='blue', alpha=0.2)
+            ax[0].plot(i, color='blue', alpha=0.2)
         ax[0].set_title("Node {}".format(context["cum_depth"]))
         ax[0].fill_between(xss, ymm + 3. * np.sqrt(ys22), ymm - 3. * np.sqrt(ys22),
-                         facecolor=[0.7539, 0.89453125, 0.62890625, 1.0], linewidth=0.5)
+                           facecolor=[0.7539, 0.89453125, 0.62890625, 1.0], linewidth=0.5)
         ax[0].plot(xss, ym, color='red', label="Prediction")
         ax[0].legend()
-        rmse_list = [t[1] for t in rmseData]
+        rmse_list = [t[1] for t in rmse_data]
         ax[1].hist(rmse_list, bins=100)
         ax[1].vlines(np.mean(rmse_list), 0, 2, color="red")
         ax[1].set_xlabel("RMSE")
         ax[1].set_ylabel("#")
         # plt.show(block=True)
 
-    return rmseData, hyperparams, model2
+    return rmse_data, hyperparams, model2
 
 
 def hierarchical_step(series, split_rmse=None, split_avgrmse=None, split_ratio=None,
                       max_avgrmse=None, min_size=None,
-                      weighted=True, plot=False, context=None, optimization_params=None, signed=False, sample=None):
+                      weighted=True, signed=False, sample=None,
+                      plot=False, context=None, optimization_params=None):
     """
     aux method for the clustering which divides the clusterlist further into clusters using a certain threshold.
 
@@ -126,9 +141,11 @@ def hierarchical_step(series, split_rmse=None, split_avgrmse=None, split_ratio=N
     :param split_avgrmse: Split on the average rmse (or split_avgrms*avgrmse if a number is given)
     :param max_avgrmse: mean similarity threshold to divide the clusters, otherwise do not split
     :param min_size: minimum cluster size, otherwise do not split
-    :param splitratio: ratio of timeseries that will be devided into the left and right cluster (optional)
+    :param split_ratio: ratio of timeseries that will be devided into the left and right cluster (optional)
+    :param weighted: Weight RMSE based on confidence of GP prediction
     :param plot: Plot the intermediate steps of the algorithm
     :param context: For internal use.
+    :param optimization_params: Dict with optional fields initialHyperParameters, bounds, method
     :param signed: Use positive and negative rmse based on whether the prediction is on average lower or higher
         than the actual series
     :param sample: Learn only on a subset of the data (speeds up learning). This can be a number (e.g., 0.8) or a
@@ -137,19 +154,19 @@ def hierarchical_step(series, split_rmse=None, split_avgrmse=None, split_ratio=N
     """
     labels, values_x, values_y = series
 
-    listRMSE, hyperparams, model = calculate_rmse_gp(values_x, values_y, weighted=weighted, plot=plot, context=context,
-                                                     optimization_params=optimization_params, signed=signed,
-                                                     sample=sample)
-    sortedListRMSE = sorted(listRMSE, key=lambda x: x[1])
-    mean_rmse = np.mean([t[1] for t in sortedListRMSE])
+    list_rmse, hyperparams, model = calculate_rmse_gp(values_x, values_y, weighted=weighted, plot=plot, context=context,
+                                                      optimization_params=optimization_params, signed=signed,
+                                                      sample=sample)
+    sorted_list_rmse = sorted(list_rmse, key=lambda x: x[1])
+    mean_rmse = np.mean([t[1] for t in sorted_list_rmse])
     if signed:
-        mean_rmse_abs = np.mean([abs(t[1]) for t in sortedListRMSE])
-        logger.info("Split at node, RMSE = [{}, {}/{}, {}]".format(sortedListRMSE[0][1], mean_rmse,
-                                                                   mean_rmse_abs, sortedListRMSE[-1][1]))
+        mean_rmse_abs = np.mean([abs(t[1]) for t in sorted_list_rmse])
+        logger.info("Split at node, RMSE = [{}, {}/{}, {}]".format(sorted_list_rmse[0][1], mean_rmse,
+                                                                   mean_rmse_abs, sorted_list_rmse[-1][1]))
     else:
         mean_rmse_abs = mean_rmse
-        logger.info("Split at node, RMSE = [{}, {}, {}]".format(sortedListRMSE[0][1], mean_rmse, sortedListRMSE[-1][1]))
-
+        logger.info("Split at node, RMSE = [{}, {}, {}]".format(sorted_list_rmse[0][1], mean_rmse,
+                                                                sorted_list_rmse[-1][1]))
 
     if max_avgrmse is not None and mean_rmse_abs < max_avgrmse:
         logger.debug('Avg RMSE too small, stopping')
@@ -169,10 +186,10 @@ def hierarchical_step(series, split_rmse=None, split_avgrmse=None, split_ratio=N
     cluster_right_y = []
 
     if split_avgrmse is not None:
-        mean_rmse = np.mean([t[1] for t in sortedListRMSE])
+        mean_rmse = np.mean([t[1] for t in sorted_list_rmse])
         if type(split_avgrmse) in [int, float]:
             mean_rmse *= split_avgrmse
-        for i, cur_rmse in sortedListRMSE:
+        for i, cur_rmse in sorted_list_rmse:
             if cur_rmse <= mean_rmse:
                 cluster_left_l.append(labels[i])
                 cluster_left_x.append(values_x[i])
@@ -183,18 +200,18 @@ def hierarchical_step(series, split_rmse=None, split_avgrmse=None, split_ratio=N
                 cluster_right_y.append(values_y[i])
     elif split_ratio is not None:
         # Split based on a ratio between clusters
-        cluster_size_length = int(math.ceil(split_ratio * len(sortedListRMSE)))
-        for idx, _ in sortedListRMSE[-cluster_size_length:]:
+        cluster_size_length = int(math.ceil(split_ratio * len(sorted_list_rmse)))
+        for idx, _ in sorted_list_rmse[-cluster_size_length:]:
             cluster_left_l.append(labels[idx])
             cluster_left_x.append(values_x[idx])
             cluster_left_y.append(values_y[idx])
-        for idx, _ in sortedListRMSE[:len(sortedListRMSE)-cluster_size_length]:
+        for idx, _ in sorted_list_rmse[:len(sorted_list_rmse) - cluster_size_length]:
             cluster_left_l.append(labels[idx])
             cluster_left_x.append(values_x[idx])
             cluster_left_y.append(values_y[idx])
     elif split_rmse is not None:
         # Split based on RMSE
-        for i, cur_rmse in sortedListRMSE:
+        for i, cur_rmse in sorted_list_rmse:
             if cur_rmse <= split_rmse:
                 cluster_left_l.append(labels[i])
                 cluster_left_x.append(values_x[i])
@@ -245,12 +262,11 @@ def hierarchical_rec(series, max_depth=None, depth=0, context=None, optimization
     return ClusterNode(left, right, model, hyperparams, depth)
 
 
-def hierarchical(series, max_depth=None, optimization_params=None, **kwargs):
+def hierarchical(series, max_depth=None, **kwargs):
     """Hierarchical clustering
 
     :param series: [label, vectorX, vectorY]
     :param max_depth: Max tree depth
-    :param optimization_params: Dict with optional fields initialHyperParameters, bounds, method
     :param kwargs: Args for hierarchical_rec
     :return: (series_left, series_right, model, hyperparams)
     """
@@ -263,10 +279,10 @@ def hierarchical(series, max_depth=None, optimization_params=None, **kwargs):
 def print_hierarchical_tree(cluster, cluster_idx=0, output=sys.stdout):
     if type(cluster) == ClusterLeaf:
         labels = [str(l) for l in sorted(cluster.series[0])]
-        print("{}Cluster {}: {}".format("  "*cluster.depth, cluster_idx, " ".join(labels)), file=output)
+        print("{}Cluster {}: {}".format("  " * cluster.depth, cluster_idx, " ".join(labels)), file=output)
         return cluster_idx + 1
     elif type(cluster) == ClusterNode:
-        print("{}Node left".format("  "*cluster.depth), file=output)
+        print("{}Node left".format("  " * cluster.depth), file=output)
         cluster_idx = print_hierarchical_tree(cluster.left, cluster_idx=cluster_idx, output=output)
         print("{}Node right".format("  " * cluster.depth), file=output)
         cluster_idx = print_hierarchical_tree(cluster.right, cluster_idx=cluster_idx, output=output)
@@ -296,15 +312,15 @@ def test():
     series_x = []
     series_y = []
     # Fill the x-values of the time-series with a time between 0 and 20
-    for i in range(0,4):
-        series_x.append(np.array(range(0,20)))
+    for i in range(0, 4):
+        series_x.append(np.array(range(0, 20)))
     print(series_x)
 
     # Fill the y-values of the time-series (actual values)
-    series_y.append(np.array(range(3,23)))
-    series_y.append(np.array(range(4,24)))
-    series_y.append(np.array([2,2,2,2,2,2,2,2,2,2,2,13,14,15,16,17,18,19,20,21]))
-    series_y.append(np.array([3,3,3,3,3,3,3,3,3,3,3,11,12,13,14,15,16,17,18,19]))
+    series_y.append(np.array(range(3, 23)))
+    series_y.append(np.array(range(4, 24)))
+    series_y.append(np.array([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 13, 14, 15, 16, 17, 18, 19, 20, 21]))
+    series_y.append(np.array([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 11, 12, 13, 14, 15, 16, 17, 18, 19]))
 
     # Normalize, focus on shape
     series_y = [preprocessing.scale(v, axis=1) for v in series_y]
@@ -312,7 +328,7 @@ def test():
     # show input timeseries
     label = 0
     for i in series_y:
-        plt.plot(i,label='timeries'+str(label))
+        plt.plot(i, label='timeries' + str(label))
         label += 1
     plt.legend()
     plt.show()
@@ -325,4 +341,3 @@ def test():
 
 if __name__ == "__main__":
     test()
-
